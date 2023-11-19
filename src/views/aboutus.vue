e3374debad607bad262075ac176453bc3c8ede03
LexiHsieh <mttw.lexihsieh@gmail.com>
2023-11-19 16:40:31 +0800

    feat: add aboutus .vue/.scss

--- src/views/aboutus.vue
+++ src/views/aboutus.vue
@@ -1,15 +1,18 @@
 <template>
     <frontnav></frontnav>
+    <front_aboutusMain> </front_aboutusMain>
     <frontfooter></frontfooter>
 </template>
 
 <script>
 import frontnav from '@/components/front_header.vue'
+import front_aboutusMain from '@/components/front_aboutusMain.vue'
 import frontfooter from '@/components/front_footer.vue'
 
 export default {
     components: {
         frontnav,
+        front_aboutusMain,
         frontfooter,
     }
 }
