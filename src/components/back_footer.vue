6efc1b501c3449f0584043fde0c3bb4e5ae87647
LexiHsieh <mttw.lexihsieh@gmail.com>
2023-11-23 17:43:30 +0800

    fix:switch backfooter to component

--- src/components/back_footer.vue
+++ src/components/back_footer.vue
@@ -1,5 +1,16 @@
 <template>
-  <footer class="frontfooter">
+  <footer class="backfooter">
+    <div class="backfooter_frame">
+      <router-link :to="{ name: 'home' }"><img src="../assets/images/common/logo.svg" alt="網中愚商標"></router-link>
+      <div class="backfooter_text">
+        <p class="backfooter_text"> 本網站為緯育TibaMe【第87期】前端工程師專業技術養成班學員作品,僅供學習、展示之用途。<br>
+          參考資源：回收大百科、Stick Hero、MyGoPen、Médecins Sans Frontières</p>
+        <router-link :to="{ name: 'home' }"><span>返回前台</span></router-link>
+      </div>
+    </div>
+  </footer>
+
+  <!-- <footer class="frontfooter">
     <div class="frontfooter_frame">
       <router-link :to="{ name: 'home' }"><img src="../assets/images/common/logo.svg" alt="網中愚商標"></router-link>
       <div class="frontfooter_text">
@@ -8,5 +19,5 @@
         <router-link :to="{ name: 'home' }"><span>- 返回前台 -</span></router-link>
       </div>
     </div>
-  </footer>
+  </footer> -->
 </template>
